74acad20b384d6d9ed2761d8d5ae24f328f6552c
Martin_MMC <204815956+MartinMMCyt@users.noreply.github.com>
2026-08-23 15:06:03 -0700

    initial commit

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = "gn-science-cache-v3";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/404.html",
+    "/favicon.ico",
+    "/domains.json",
+    "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", event => {
+    if (event.request.method !== "GET") return;
+
+    event.respondWith(
+        caches.open(CACHE_NAME).then(async cache => {
+            const cachedResponse = await cache.match(event.request, { ignoreSearch: true });
+
+            const fetchPromise = fetch(event.request).then(networkResponse => {
+                if (networkResponse && networkResponse.ok) {
+                    cache.put(event.request, networkResponse.clone());
+                }
+                return networkResponse;
+            }).catch(() => {
+            });
+
+            return cachedResponse || fetchPromise;
+        })
+    );
+});
